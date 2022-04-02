d2b3c341401e05e35a7b9be921cd2b7b130ed3e9
diegosampaio <diego.estaleiro@gmail.com>
2022-04-02 11:23:12 -0300

    Atualização migration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -18,8 +18,7 @@ class CreateUsersTable extends Migration
             $table->string('name');
             $table->string('email')->unique();
             $table->string('photo')->nullable();
-            $table->string('phone');
-            $table->timestamp('email_verified_at')->nullable();
+            $table->string('phone')->nullable();
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
